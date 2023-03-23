4d65c6ee4474ab91347ea1c57bf8123fc46a0612
Josh <joshuastolle445@gmail.com>
2023-03-23 12:31:58 +0100

    create Repo

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
 import { createServer } from "node:http";
+import Chance from "chance";
 
-// export const server = …
+const chance = new Chance();
+
+console.log(chance.name());
+console.log(chance.age());
+console.log(chance.profession());
+
+export const server = createServer((request, response) => {
+  if (request.url === "/") {
+    response.end(
+      `Hello, my name is ${chance.name()} and I am ${chance.age()} years old. I am a ${chance.profession()}.`
+    );
+  } else {
+    response.statusCode = 404;
+    response.end("Not found");
+  }
+});
